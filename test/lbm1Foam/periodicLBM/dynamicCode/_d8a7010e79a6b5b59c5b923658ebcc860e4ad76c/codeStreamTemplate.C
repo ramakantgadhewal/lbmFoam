4f31cf7b9d69c8e6c53cdb079be67c8a11e42f24
PoliMala <andrea.malatesta@mail.polimi.it>
2022-10-21 23:59:07 +0200

    modified lbm1Foam test directory to contain more than one case

---
+++ test/lbm1Foam/periodicLBM/dynamicCode/_d8a7010e79a6b5b59c5b923658ebcc860e4ad76c/codeStreamTemplate.C
@@ -0,0 +1,93 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) YEAR OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Description
+    Template for use with codeStream.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dictionary.H"
+#include "Ostream.H"
+#include "Pstream.H"
+#include "unitConversion.H"
+
+//{{{ begin codeInclude
+#line 23 "/home/mala/OpenFOAM/mala-9/platforms/linux64GccDPInt32Opt/src/solvers/lbmFoam/test/lbm1Foam/periodicLBM/0/U/#codeStream"
+#include "fvCFD.H"
+//}}} end codeInclude
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+extern "C"
+{
+    void codeStream_d8a7010e79a6b5b59c5b923658ebcc860e4ad76c
+    (
+        Ostream& os,
+        const dictionary& dict
+    )
+    {
+//{{{ begin code
+        #line 40 "/home/mala/OpenFOAM/mala-9/platforms/linux64GccDPInt32Opt/src/solvers/lbmFoam/test/lbm1Foam/periodicLBM/0/U/#codeStream"
+// profile parameters declaration
+        scalar IwidthU2(4);
+        scalar maxU(1);
+        scalar xLoc;
+        scalar uX;
+
+        const IOdictionary& d = static_cast<const IOdictionary&>(dict);
+        const fvMesh& mesh = refCast<const fvMesh>(d.db());
+
+        // initialize field
+        vectorField U(mesh.nCells(), vector(0.,0.,0.));
+        Info<<"1:here yes"<< endl;
+        forAll(U, cI)
+        {
+          xLoc = mesh.C()[cI].x();
+          uX = maxU*exp(-xLoc*xLoc*IwidthU2);
+          U[cI] = vector(uX,0.,0.);
+        }
+        Info<<"2:here yes"<< endl;
+        writeEntry(os, "", U);
+//}}} end code
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
+
